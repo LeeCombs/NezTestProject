7a869c464dee0e5ec4a0960e68867b91e11f84c3
HellaBored <leecombs_30@hotmail.com>
2018-04-13 17:28:26 -0700

    Init Commit

---
+++ NezTestProject/Game1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using Nez;
+
+namespace NezTestProject
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Core
+    {
+        public Game1() : base(width: 1280, height: 768, isFullScreen: false, enableEntitySystems: false)
+        {
+            //
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            Window.AllowUserResizing = true;
+            var myScene = Scene.createWithDefaultRenderer(Color.CornflowerBlue);
+
+            var entityOne = myScene.createEntity("entity-one");
+            var entityTwo = myScene.createEntity("entity-two");
+
+            scene = myScene;
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            // spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+        
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+        
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
