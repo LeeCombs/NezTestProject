d263a1c5fa17124bfa396f5803098f121e1756d5
HellaBored <leecombs_30@hotmail.com>
2018-04-29 15:41:17 -0700

    .

---
+++ NezTestProject/EnemyEntity.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Nez.Sprites;
+
+namespace NezTestProject {
+    class EnemyEntity : Nez.Entity {
+        public EnemyEntity(Vector2 position, Texture2D texture) {
+            addComponent(new Sprite(texture));
+            addComponent(new SimpleMover());
+        }
+    }
+}
---
+++ NezTestProject/EnemyManager.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace NezTestProject {
+    class EnemyManager {
+        public enum Enemies {
+            Base, Goomba, Bat
+        }
+
+        public EnemyEntity MakeEnemy(Enemies enemy, Vector2 position)
+        {
+            // grab texture based on supplied enemy
+            Texture2D enemyTexture = Nez.Core.content.Load<Texture2D>("Graphics\\Bomb");
+            var ent = new EnemyEntity(position, enemyTexture);
+            
+            switch (enemy) {
+                case Enemies.Base:
+                    //
+                    break;
+                case Enemies.Goomba:
+                    // load Goomba texture
+                    // 
+                    break;
+                case Enemies.Bat:
+                    // load Bat texture
+                    // ent.addComponent(new FlyingComponent());
+                    break;
+                default:
+                    // Error
+                    break;
+            }
+            
+            return ent;
+        }
+    }
+}
